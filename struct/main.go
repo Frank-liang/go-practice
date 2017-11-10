8d6097952cd311bc68a44ccd0fd418eccbab56d5
root <root@zcool_manager.(none)>
2017-11-10 17:26:33 +0800

    New struct

--- struct/main.go
+++ struct/main.go
@@ -1,27 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
-type Students struct {
-	Id   int
-	Name string
+type Teacher struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+type Student struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
-type Teachers struct {
-	Id   int
-	Name string
-	Sex  string
+type Class struct {
+	teachers []Teacher `json:"teachers"`
+	students []Student `json:"students"`
 }
 
 func main() {
-	var s Students
-	s.Id = 1
-	s.Name = "jack"
-	fmt.Println(s)
-	s1 := Teachers{
-		Id:   2,
-		Name: "alice",
-		Sex:  "M",
+	for {
+		fmt.Printf("Input Name and Age: ")
+		var name string
+		var age int
+		var s2 Student
+		fmt.Scanf("%s%d", &name, &age)
+		s1 := Student{
+			Id:   1,
+			Name: name,
+			Age:  age,
+		}
+		fmt.Println(s1)
+		buf, err := json.Marshal(s1)
+		if err != nil {
+			log.Fatalf("marshal error:%s", err)
+		}
+		fmt.Println(string(buf))
+
+		err = json.Unmarshal(buf, &s2)
+		if err == nil {
+			log.Println(s2)
+		}
 	}
-	fmt.Println(s1)
 }
