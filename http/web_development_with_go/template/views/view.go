aa4d40261e4dbd62153c137a794fb3126d8d1751
root <root@zcool_manager.(none)>
2018-03-13 15:41:50 +0800

    use path/filepath Glob instead of separate gohtml

--- http/web_development_with_go/template/views/view.go
+++ http/web_development_with_go/template/views/view.go
@@ -1,12 +1,25 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml")
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
