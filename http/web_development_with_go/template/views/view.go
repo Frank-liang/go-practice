0992545fe769b93afcc2388a24c44dafdf0fbbe6
root <root@zcool_manager.(none)>
2018-03-13 15:22:58 +0800

    use bootstracp footer navbar seperately

--- http/web_development_with_go/template/views/view.go
+++ http/web_development_with_go/template/views/view.go
@@ -5,7 +5,8 @@ import "html/template"
 func NewView(layout string, files ...string) *View {
 	files = append(files,
 		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml")
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/navbar.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
