abf092ca949514c7e542bf0881d4cd0e1ca70ef8
Frank-liang <guishiwho@163.com>
2018-04-12 22:51:04 +0800

    Not finished

--- practice/adventure_stories/cyoa/story.go
+++ practice/adventure_stories/cyoa/story.go
@@ -1,6 +1,20 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type Story map[string]Chapter
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
 
 type Chapter struct {
 	Title      string   `json:"title"`
