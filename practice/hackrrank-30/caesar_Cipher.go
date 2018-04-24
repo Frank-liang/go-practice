7e10bceb9f9e14cd58468e2ca7e4ba93da77fa52
Frank-liang <guishiwho@163.com>
2018-04-24 23:05:29 +0800

    Caesar Cipher, need to understand

---
+++ practice/hackrrank-30/caesar_Cipher.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var length, delta int
+	var input string
+	fmt.Scanf("%d\n", &length)
+	fmt.Scanf("%s\n", &input)
+	fmt.Scanf("%d\n", &delta)
+
+	var ret []rune
+	for _, ch := range input {
+		ret = append(ret, cipher(ch, delta))
+	}
+	fmt.Println(string(ret))
+}
+
+func cipher(r rune, delta int) rune {
+	if r >= 'A' && r <= 'Z' {
+		return rotate(r, 'A', delta)
+	}
+	if r >= 'a' && r <= 'z' {
+		return rotate(r, 'a', delta)
+	}
+	return r
+}
+
+func rotate(r rune, base, delta int) rune {
+	tmp := int(r) - base
+	tmp = (tmp + delta) % 26
+	return rune(tmp + base)
+}
