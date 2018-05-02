e8d5aaa64932cf1cfb48bea719194e5406cd3b43
Frank-liang <guishiwho@163.com>
2018-05-02 22:44:23 +0800

    Add CLI task

---
+++ practice/CLI_task/cmd/root.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "task",
+	Short: "Task is a CLI task manager",
+}
---
+++ practice/CLI_task/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Frank-liang/go/practice/CLI_task/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+
+}
