86bf402566a2a7394d71a79e867ba1c7385e2b96
Frank-liang <guishiwho@163.com>
2018-06-05 16:49:39 +0800

    Remember token normalizer

--- http/web_development_with_go/template/models/users.go
+++ http/web_development_with_go/template/models/users.go
@@ -114,6 +114,14 @@ type userGorm struct {
 
 type userValFn func(*User) error
 
+func (uv *userValidator) hmacRemember(user *User) error {
+	if user.Remember == "" {
+		return nil
+	}
+	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
+}
+
 func runUserValFns(user *User, fns ...userValFn) error {
 	for _, fn := range fns {
 		if err := fn(user); err != nil {
@@ -174,11 +182,6 @@ func (uv *userValidator) bcryptPassword(user *User) error {
 }
 
 func (uv *userValidator) Create(user *User) error {
-	if err := runUserValFns(user,
-		uv.bcryptPassword); err != nil {
-		return err
-	}
-
 	if user.Remember == "" {
 		token, err := rand.RememberToken()
 		if err != nil {
@@ -186,7 +189,14 @@ func (uv *userValidator) Create(user *User) error {
 		}
 		user.Remember = token
 	}
-	user.RememberHash = uv.hmac.Hash(user.Remember)
+
+	err := runUserValFns(user,
+		uv.bcryptPassword,
+		uv.hmacRemember)
+	if err != nil {
+		return err
+	}
+
 	return uv.UserDB.Create(user)
 }
 
@@ -196,12 +206,11 @@ func (ug *userGorm) Create(user *User) error {
 
 // Update will hash a remember token if it is provided.
 func (uv *userValidator) Update(user *User) error {
-	if err := runUserValFns(user,
-		uv.bcryptPassword); err != nil {
+	err := runUserValFns(user,
+		uv.bcryptPassword,
+		uv.hmacRemember)
+	if err != nil {
 		return err
-	}
-	if user.Remember != "" {
-		user.RememberHash = uv.hmac.Hash(user.Remember)
 	}
 	return uv.UserDB.Update(user)
 }
@@ -280,6 +289,11 @@ func (ug *userService) Authenticate(email, password string) (*User, error) {
 }
 
 func (uv *userValidator) ByRemember(token string) (*User, error) {
-	rememberHash := uv.hmac.Hash(token)
-	return uv.UserDB.ByRemember(rememberHash)
+	user := User{
+		Remember: token,
+	}
+	if err := runUserValFns(&user, uv.hmacRemember); err != nil {
+		return nil, err
+	}
+	return uv.UserDB.ByRemember(user.RememberHash)
 }
