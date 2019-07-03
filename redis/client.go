832ab1bf7ad278e862706642c8dd86c4f03965b7
Gogs <gogs@fake.local>
2019-07-03 15:30:07 +0800

    Add redis lib based on go-redis/redis

---
+++ redis/client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+var (
+	client = &redisClient{}
+)
+
+type redisClient struct {
+	c *redis.Client
+}
+
+func initialize() *redisClient {
+	c := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+	})
+
+	if err := c.Ping().Err(); err != nil {
+		panic("Unable to connect to redis " + err.Error())
+	}
+
+	client.c = c
+	return client
+
+}
+
+func (client *redisClient) getKey(key string, src interface{}) error {
+	val, err := client.c.Get(key).Result()
+	if err == redis.Nil || err != nil {
+		return err
+	}
+	err = json.Unmarshal([]byte(val), &src)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (client *redisClient) setKey(key string, value interface{}, expiration time.Duration) error {
+	cacheEntry, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	err = client.c.Set(key, cacheEntry, expiration).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("ok")
+}
