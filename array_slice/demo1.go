609cc3f09d652bc666e962338b781c7b4884b23e
Gogs <gogs@fake.local>
2019-07-03 16:13:03 +0800

    Add two array/slice examples

---
+++ array_slice/demo1.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 示例1。
+	s6 := make([]int, 0)
+	fmt.Printf("The capacity of s6: %d\n", cap(s6))
+	for i := 1; i <= 5; i++ {
+		s6 = append(s6, i)
+		fmt.Println(s6)
+		fmt.Printf("s6(%d): len: %d, cap: %d\n", i, len(s6), cap(s6))
+	}
+	fmt.Println()
+
+	// 示例2。 cap值超过1024以1.25倍增加。1024以下 1倍增加
+	s7 := make([]int, 1024)
+	fmt.Printf("The capacity of s7: %d\n", cap(s7))
+	s7e1 := append(s7, make([]int, 200)...)
+	fmt.Printf("s7e1: len: %d, cap: %d\n", len(s7e1), cap(s7e1))
+	s7e2 := append(s7, make([]int, 400)...)
+	fmt.Printf("s7e2: len: %d, cap: %d\n", len(s7e2), cap(s7e2))
+	s7e3 := append(s7, make([]int, 600)...)
+	fmt.Printf("s7e3: len: %d, cap: %d\n", len(s7e3), cap(s7e3))
+	fmt.Println()
+
+	// 示例3。
+	s8 := make([]int, 10)
+	fmt.Printf("The capacity of s8: %d\n", cap(s8))
+	s8a := append(s8, make([]int, 11)...)
+	fmt.Printf("s8a: len: %d, cap: %d\n", len(s8a), cap(s8a))
+	s8b := append(s8a, make([]int, 23)...)
+	fmt.Printf("s8b: len: %d, cap: %d\n", len(s8b), cap(s8b))
+	s8c := append(s8b, make([]int, 45)...)
+	fmt.Printf("s8c: len: %d, cap: %d\n", len(s8c), cap(s8c))
+
+	//example 5
+	s9 := make([]int, 8)
+	fmt.Println(s9)
+	s10 := []int{1, 2, 3, 4, 5, 6, 6, 7}
+	fmt.Println(s10)
+	s11 := s10[:3] //if s10[:3] change into s10[3:], then check the result carefully, expecially "cap"
+	fmt.Println(s11)
+	fmt.Printf("s11: len: %d, cap: %d\n", len(s11), cap(s11))
+}
